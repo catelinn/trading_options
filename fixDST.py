6635426b518e7896ac7b1817171be7278bc3dc36
Catelinn <catelinn.x@gmail.com>
2022-03-18 00:13:54 -0700

    fix updatetime error since 2022 DST

---
+++ fixDST.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from datetime import datetime, timedelta
+
+f = 'outputs/signals_marketclose.csv'
+df = pd.read_csv(f)
+
+isDST = df['date'].str.contains('-07:00')
+df.loc[isDST, 'date'] = pd.to_datetime(df.loc[isDST,'date'])-pd.DateOffset(hours=1)
+
+print(df.loc[isDST, 'date'])
+df.to_csv(f)
