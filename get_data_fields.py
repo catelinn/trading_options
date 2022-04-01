79db97160d73b54218308363832fab2bc6f1339e
Catelinn <catelinn.x@gmail.com>
2022-04-01 12:24:15 -0700

    make field extraction generic for any stream service

--- get_data_fields.py
+++ get_data_fields.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup as bs
 from matplotlib.collections import EventCollection
 import pandas as pd
 
+# Go to TDA streaming data guide webpage, use inspect tool to find field definitions table of a service
+# copy the table elemnt and save the html to 'fields.html' for use in this script
 with open('fields.html') as f:
 
     html = f.read()
@@ -27,10 +29,17 @@ def extractText(cell):
     except:
         pass
 
-data = []
-for row in table.findAll('tr'):
-    cells = row.findAll('td')
-    data.append([extractText(cell) for cell in cells])
 
-df = pd.DataFrame(columns=data[0], data=data[1:])
-df.to_csv('QUOTE_fields.csv')
+def main(service):
+    '''
+    Extract fields definitions for specific service and save to csv
+    '''
+    data = []
+    for row in table.findAll('tr'):
+        cells = row.findAll('td')
+        data.append([extractText(cell) for cell in cells])
+
+    df = pd.DataFrame(columns=data[0], data=data[1:])
+    df.to_csv(f'fields_{service}.csv')
+
+main('TIMESALE')
